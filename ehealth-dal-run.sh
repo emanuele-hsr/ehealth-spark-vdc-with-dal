d2282592d3e220ccdcd96d3a144a4219fb657c51
Emanuele Falzone <falzone.emanuele@hsr.it>
2019-10-30 14:07:30 +0100

    Update DAL Dockerfile to handle env variables

---
+++ ehealth-dal-run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+MYSQL_URI=$(echo $MYSQL_URI | sed -e "s,&,\\\&,")
+
+sed -e "s,#{MINIO_URI},$MINIO_URI,g" \
+    -e "s,#{MINIO_ACCESS_KEY},$MINIO_ACCESS_KEY,g" \
+    -e "s,#{MINIO_SECRET_KEY},$MINIO_SECRET_KEY,g" \
+    -e "s,#{MYSQL_URI},$MYSQL_URI,g" \
+    -e "s,#{MYSQL_USERNAME},$MYSQL_USERNAME,g" \
+    -e "s,#{MYSQL_PASSWORD},$MYSQL_PASSWORD,g" \
+    -e "s,#{POLICY_ENFORCEMENT_URI},$POLICY_ENFORCEMENT_URI,g" \
+    -e "s,#{KEYCLOAK_PUBLIC_KEY_URI},$KEYCLOAK_PUBLIC_KEY_URI,g" \
+    /app/config/ehealth-dal-config.yml.sed > /app/config/ehealth-dal-config.yml
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+_int() {
+  echo "Caught SIGINT signal!"
+  kill -INT "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+trap _int SIGINT
+
+/app/dist/ehealth-dal-0.1/bin/ehealth-server /app/config/ehealth-dal-config.yml &
+
+child=$!
+wait "$child"
